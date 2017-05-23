4cb0d453e9bc76f67af4da3668e270541fe0c3c6
Wonwoo Choi <chwo9843@gmail.com>
2017-05-23 16:27:38 +0900

    Make Slack integration optional

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ fs.writeFileAsync('pid', process.pid.toString()).catch(err => {
   process.exit(1);
 });
 
-const botToken = process.env.SLACK_BOT_TOKEN || '';
+const botToken = process.env.SLACK_BOT_TOKEN || null
 const apiKey = process.env.MAILGUN_API_KEY || null;
 const verifier = apiKey ? new MailgunVerifier(apiKey) : null;
-const web = new WebClient(botToken);
+const web = botToken ? new WebClient(botToken) : null;
 
+if (web === null) {
+  console.log('SLACK_BOT_TOKEN not given, not relaying to Slack');
+}
 if (verifier === null) {
   console.log('MAILGUN_API_KEY not given, not verifying webhooks');
 }
@@ -35,26 +38,28 @@ app.use(async ctx => {
     ctx.assert(valid, 400);
   }
 
-  const subject = fields.get('subject');
-  const from = fields.get('from');
-  const text = fields.get('stripped-text');
-  web.chat.postMessage(
-    '#random',
-    '메일이 도착했습니다.',
-    {
-      link_names: false,
-      as_user: true,
-      attachments: [
-        {
-          fallback: subject,
-          author_name: from,
-          title: subject,
-          text: text,
-          ts: (new Date().getTime() / 1000 | 0).toString()
-        }
-      ]
-    }
-  );
+  if (web !== null) {
+    const subject = fields.get('subject');
+    const from = fields.get('from');
+    const text = fields.get('stripped-text');
+    web.chat.postMessage(
+      '#random',
+      '메일이 도착했습니다.',
+      {
+        link_names: false,
+        as_user: true,
+        attachments: [
+          {
+            fallback: subject,
+            author_name: from,
+            title: subject,
+            text: text,
+            ts: (new Date().getTime() / 1000 | 0).toString()
+          }
+        ]
+      }
+    );
+  }
   ctx.status = 200;
 });
 
