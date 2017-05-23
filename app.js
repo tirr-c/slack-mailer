4eec48862fa079f6d3c3a2eb1e97fcfacb0cf2cd
Wonwoo Choi <chwo9843@gmail.com>
2017-05-23 16:20:10 +0900

    Integrate with Slack

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ fs.writeFileAsync('pid', process.pid.toString()).catch(err => {
 const botToken = process.env.SLACK_BOT_TOKEN || '';
 const apiKey = process.env.MAILGUN_API_KEY || null;
 const verifier = apiKey ? new MailgunVerifier(apiKey) : null;
-// const web = new WebClient(botToken);
+const web = new WebClient(botToken);
+
 if (verifier === null) {
   console.log('MAILGUN_API_KEY not given, not verifying webhooks');
 }
@@ -34,6 +35,26 @@ app.use(async ctx => {
     ctx.assert(valid, 400);
   }
 
+  const subject = fields.get('subject');
+  const from = fields.get('from');
+  const text = fields.get('stripped-text');
+  web.chat.postMessage(
+    '#random',
+    '메일이 도착했습니다.',
+    {
+      link_names: false,
+      as_user: true,
+      attachments: [
+        {
+          fallback: subject,
+          author_name: from,
+          title: subject,
+          text: text,
+          ts: (new Date().getTime() / 1000 | 0).toString()
+        }
+      ]
+    }
+  );
   ctx.status = 200;
 });
 
